47f500abdc3f44f1b31bf055af62200ee1373f84
kamelot88 <VasilchenkoOV1988@gmail.com>
2021-10-03 21:39:15 +0300

    123

--- js/control_blog.js
+++ js/control_blog.js
@@ -1,17 +1,17 @@
 //! Реализация бургер-меню
 
-const headerBlock = document.querySelector("body > .head-blog");
-const burgerBtn = document.querySelector("#head > div.burger-blog");
+const headerBlock2 = document.querySelector("body > .head-blog");
+const burgerBtn2 = document.querySelector("#head > div.burger.burger-blog");
 const arrowBurger1 = document.querySelector("#head > div.burger > div > svg.arrow_blog1");
 const arrowBurger2 = document.querySelector("#head > div.burger > div > svg.arrow_blog2");
 
-burgerBtn.onclick = function(e) {
-   if (headerBlock.style.left != 0 + "vw") {
-      headerBlock.style.left = 0 + "vw";
+burgerBtn2.onclick = function(e) {
+   if (headerBlock2.style.left != 0 + "vw") {
+      headerBlock2.style.left = 0 + "vw";
       arrowBurger1.classList.add('rotate-arrow');
       arrowBurger2.classList.add('rotate-arrow');
    } else {
-      headerBlock.style.left = -65 + "vw";
+      headerBlock2.style.left = -65 + "vw";
       arrowBurger1.classList.remove('rotate-arrow');
       arrowBurger2.classList.remove('rotate-arrow');
    }
